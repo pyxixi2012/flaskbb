b42048e9d27b813da8031eac65777f917ab19eb5
justanr <alecreiter@gmail.com>
2015-08-14 10:03:54 -0400

    Expand birthday field tests

--- tests/unit/utils/test_fields.py
+++ tests/unit/utils/test_fields.py
@@ -2,20 +2,40 @@
 import pytest
 from wtforms.form import Form
 from flaskbb.utils.fields import BirthdayField
+from datetime import date
 
 
-def test_birthday_field():
-    class F(Form):
-        birthday = BirthdayField(format='%d %m %Y')
+class TestForm(Form):
+    birthday = BirthdayField(format='%d %m %Y')
 
-    a = ["04 02 2015"]
-    b = ["None", "None", "2015"]  # this one should fail
-    c = ["None", "None", "None"]
 
-    form = F()
+@pytest.mark.parametrize('date, expected', [
+    (["23 01 1989"], date(1989, 1, 23)),
+    (["04", "04", "2015"], date(2015, 4, 4)),
+    (["29", "02", "2004"], date(2004, 2, 29))
+])
+def test_birthday_field_with_good(date, expected):
+    f = TestForm()
+    f.birthday.process_formdata(date)
 
-    assert form.birthday.process_formdata(a) is None
-    assert form.birthday.process_formdata(c) is None
+    assert f.birthday.data == expected
 
-    with pytest.raises(ValueError):
-        form.birthday.process_formdata(b)
+
+def test_birthday_field_with_all_none():
+    f = TestForm()
+    f.birthday.process_formdata(["None"]*3)
+
+    assert f.birthday.data is None
+
+
+@pytest.mark.parametrize('date', [
+    ["None", "None", "2015"],
+    ["1989 01 23"], ["29", "02", "2013"]
+])
+def test_birthday_with_bad_data(date):
+    f = TestForm()
+
+    with pytest.raises(ValueError) as excinfo:
+        f.birthday.process_formdata(date)
+
+    assert str(excinfo.value) == "Not a valid date value"
