de9202b124b078ffe2ad9fab2ca02d94a7e91119
justanr <alecreiter@gmail.com>
2015-08-26 10:30:51 -0400

    Inherit from base werkzeug HTTPException

--- flaskbb/exceptions.py
+++ flaskbb/exceptions.py
@@ -1,7 +1,7 @@
 from werkzeug import exceptions as exc
 
 
-class FlaskBBError(Exception):
+class FlaskBBError(exc.HTTPException):
     """Base for all FlaskBB exceptions."""
     code = 500
     description = "An internal error has occured."
@@ -9,3 +9,7 @@ class FlaskBBError(Exception):
 
 class AuthorizationRequired(exc.Forbidden, FlaskBBError):
     description = "You are not authorized to access this area."
+
+
+class NotFound(exc.NotFound, FlaskBBError):
+    description = "The requested resource was not found."
