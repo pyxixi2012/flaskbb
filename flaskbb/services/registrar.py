5ccf3f8c3294196fd60e668ae38da988074ff0ab
Alec Nikolas Reiter <alecreiter@gmail.com>
2015-10-18 16:14:12 -0400

    Add AfterRegister for post registration actions

--- flaskbb/services/registrar.py
+++ flaskbb/services/registrar.py
@@ -24,3 +24,17 @@ class Registrar(object):
 
     def __call__(self, username, email, password, **kwargs):
         return self.register(username, email, password, **kwargs)
+
+
+class AfterRegister(object):
+    def __init__(self, registrar, after):
+        self._registrar = registrar
+        self._after = after
+
+    def register(self, *args, **kwargs):
+        user = self._registrar(*args, **kwargs)
+        self._after(user)
+        return user
+
+    def __call__(self, *args, **kwargs):
+        return self.register(*args, **kwargs)
