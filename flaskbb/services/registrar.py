70a5098ab56ab5243210547a288decd077dd0a0a
Alec Nikolas Reiter <alecreiter@gmail.com>
2015-10-18 16:09:15 -0400

    Return user from register method

--- flaskbb/services/registrar.py
+++ flaskbb/services/registrar.py
@@ -20,6 +20,7 @@ class Registrar(object):
         user = self._factory(username=username, email=email, password=password, **kwargs)
         self._validator(user)
         self._repository.persist(user)
+        return user
 
     def __call__(self, username, email, password, **kwargs):
-        self.register(username, email, password, **kwargs)
+        return self.register(username, email, password, **kwargs)
