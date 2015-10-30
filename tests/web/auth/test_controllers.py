7f63d8378d9e6398f562c37f0a20f48d30c837e9
Alec Nikolas Reiter <alecreiter@gmail.com>
2015-10-30 13:30:10 -0400

    Repair failing tests

--- tests/web/auth/test_controllers.py
+++ tests/web/auth/test_controllers.py
@@ -1,6 +1,7 @@
 from flaskbb.auth.controllers import RegisterUser, LoginUser
-from flaskbb.boundaries import AuthenticatorBridge
+from flaskbb.boundaries import AuthenticatorBridge, RegistrationBridge
 from flaskbb.services.authentication import Authenticator
+from flaskbb.services.registrar import Registrar
 from flaskbb.exceptions import ValidationError
 
 try:
@@ -65,47 +66,42 @@ class FakeLoginForm(FakeForm):
         return {k: v for k, v in errors.items() if v}
 
 
-def stingy_registrar(username, email, password):
-    raise ValidationError('Fails validation', 'username')
-
-
 class TestRegisterUser(object):
     def setup(self):
         self.form = FakeRegisterForm('fred', 'fred', 'fred')
-        self.generous_registrar = lambda *a, **k: None
-        self.stingy_registrar = stingy_registrar
+        self.registrar = mock.create_autospec(Registrar)
+        self.controller = RegisterUser(
+            self.form,
+            lambda l: RegistrationBridge(self.registrar, l),
+            None, None)
 
     def test_submit_invalid_form_data(self):
         self.form.valid = False
-        controller = RegisterUser(self.form, self.generous_registrar, None, None)
 
-        with mock.patch.object(RegisterUser, '_render') as render:
-            controller.post()
+        with mock.patch.object(self.controller, '_render') as render:
+            self.controller.post()
 
         assert render.call_count == 1
 
     def test_submit_valid_form_data(self):
-        controller = RegisterUser(self.form, self.generous_registrar, None, None)
+        self.registrar.register.return_value = 'Fred'
+        with mock.patch.object(self.controller, 'registration_succeeded') as success:
+            self.controller.post()
 
-        with mock.patch.object(RegisterUser, '_register_user') as register:
-            controller.post()
-
-        assert register.call_count == 1
+        assert success.call_args == mock.call('Fred', **self.form.data)
 
     def test_fail_registration_validation(self):
-        controller = RegisterUser(self.form, self.stingy_registrar, None, None)
+        error = ValidationError('Fails', 'username')
+        self.registrar.register.side_effect = error
 
-        with mock.patch.object(RegisterUser, '_render') as render:
-            controller.post()
+        with mock.patch.object(self.controller, 'registration_failed') as failed:
+            self.controller.post()
 
-        assert self.form.errors == {'username': ['Fails validation']}
-        assert render.call_count == 1
+        assert failed.call_args == mock.call(error, **self.form.data)
 
     def test_pass_registration_validation(self):
-        controller = RegisterUser(self.form, self.generous_registrar, None, None)
-
-        with mock.patch.object(RegisterUser, '_redirect') as redirect:
-            controller.post()
+        with mock.patch.object(self.controller, '_redirect') as redirect:
+            self.controller.post()
 
         assert redirect.call_count == 1
 
@@ -114,32 +110,31 @@ class TestLoginUser(object):
     def setup(self):
         self.form = FakeLoginForm('fred', 'fred')
         self.authenticator = mock.create_autospec(Authenticator)
-        self.bridge = lambda l: AuthenticatorBridge(self.authenticator, l)
+        self.controller = LoginUser(
+            self.form, lambda l: AuthenticatorBridge(self.authenticator, l),
+            None, None)
 
     def test_submit_valid_data(self):
-        controller = LoginUser(self.form, self.bridge, None, None)
         self.authenticator.authenticate.return_value = 'Fred'
 
-        with mock.patch.object(controller, 'authentication_succeeded') as succeed:
-            controller.post()
+        with mock.patch.object(self.controller, 'authentication_succeeded') as succeed:
+            self.controller.post()
 
         assert succeed.call_args == mock.call('Fred', **self.form.data)
 
     def test_submit_invalid_data(self):
         self.form.valid = False
-        controller = LoginUser(self.form, self.bridge, None, None)
 
-        with mock.patch.object(controller, '_render') as render:
-            controller.post()
+        with mock.patch.object(self.controller, '_render') as render:
+            self.controller.post()
 
         assert render.call_count == 1
 
     def test_authenticator_fails(self):
         error = ValidationError('Failed')
         self.authenticator.authenticate.side_effect = error
-        controller = LoginUser(self.form, self.bridge, None, None)
 
-        with mock.patch.object(controller, 'authentication_failed') as failed:
-            controller.post()
+        with mock.patch.object(self.controller, 'authentication_failed') as failed:
+            self.controller.post()
 
         assert failed.call_args == mock.call(error, **self.form.data)
